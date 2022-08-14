fde4903617e0e8845e553152dfdcaa6ad370a44f
Grey601 <grev.gr@hotmail.com>
2022-08-14 17:22:09 +0300

    credit terms (years) added, reformatted

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
     public double calculate(int creditAmount, int years, double interestRate) {
         int months = years * 12;
-        double percentPerMonth = interestRate/100/12;
-        double paymentPerMonth = creditAmount*((percentPerMonth/(Math.pow((1+percentPerMonth), months)-1))+percentPerMonth);
+        double percentPerMonth = interestRate / 100 / 12;
+        double paymentPerMonth = creditAmount * ((percentPerMonth / (Math.pow((1 + percentPerMonth), months) - 1)) + percentPerMonth);
         return Math.round(paymentPerMonth);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,25 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         int creditAmount = 1_000_000;
-        int years = 3;
+        int years = 1;
         double interestRate = 9.99;
         double paymentPerMonth = service.calculate(creditAmount, years, interestRate);
+        System.out.println("Сумма кредита: " + creditAmount + " рублей");
+        System.out.println("Срок кредита: " + years + " лет/год(а)");
+        System.out.println("Ежемесячный платёж: " + paymentPerMonth + " рублей");
+        System.out.println("Процентная ставка: " + interestRate + "%");
+
+        years = 2;
+        paymentPerMonth = service.calculate(creditAmount, years, interestRate);
+        System.out.println(" ");
+        System.out.println("Сумма кредита: " + creditAmount + " рублей");
+        System.out.println("Срок кредита: " + years + " лет/год(а)");
+        System.out.println("Ежемесячный платёж: " + paymentPerMonth + " рублей");
+        System.out.println("Процентная ставка: " + interestRate + "%");
+
+        years = 3;
+        paymentPerMonth = service.calculate(creditAmount, years, interestRate);
+        System.out.println(" ");
         System.out.println("Сумма кредита: " + creditAmount + " рублей");
         System.out.println("Срок кредита: " + years + " лет/год(а)");
         System.out.println("Ежемесячный платёж: " + paymentPerMonth + " рублей");
